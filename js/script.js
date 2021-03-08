04f9a0f069c959a89430969072b43013c59ebce6
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-03-08 14:47:51 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 10000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    time: 10000,
+    nav: true,
+  });
+}
